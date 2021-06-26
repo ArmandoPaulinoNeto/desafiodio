5c6f3eb024624ed3c3b72ac739244528457e53e3
Armando <armandopaulinosilvaneto@gmail.com>
2021-06-26 13:38:20 -0300

    Criação de classes e alteração.

--- src/main/java/br/com/desafiodio/personapi/controller/PersonController.java
+++ src/main/java/br/com/desafiodio/personapi/controller/PersonController.java
@@ -1,14 +1,25 @@
 package br.com.desafiodio.personapi.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import br.com.desafiodio.personapi.dto.response.MessageResponseDTO;
+import br.com.desafiodio.personapi.entity.Person;
+import br.com.desafiodio.personapi.repository.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/people")
 public class PersonController {
-    @GetMapping
-    public String getBook(){
-        return "Welcome to API test!";
+
+    private PersonRepository personRepository;
+
+    @Autowired
+    public PersonController(PersonRepository personRepository){
+        this.personRepository = personRepository;
+    }
+
+    @PostMapping
+    public MessageResponseDTO createPerson(@RequestBody Person person){
+        Person savePerson = personRepository.save(person);
+        return MessageResponseDTO.builder().message("Pessoa inserida com sucesso" + savePerson.getId()).build();
     }
 }
