b56d7462d0b4405f2a56eb3d7200503a45cba7c1
Armando <armandopaulinosilvaneto@gmail.com>
2021-06-25 21:13:31 -0300

    Inciando o projeto no Github.

--- src/main/java/br/com/desafiodio/personapi/PersonapiApplication.java
+++ src/main/java/br/com/desafiodio/personapi/PersonapiApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PersonapiApplication {
-
+//https://people-desafiodio.herokuapp.com/api/v1/people
 	public static void main(String[] args) {
 		SpringApplication.run(PersonapiApplication.class, args);
 	}
