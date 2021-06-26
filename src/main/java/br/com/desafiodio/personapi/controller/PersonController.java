5d764e6be9efb265264dd4e38f8b2ce8a68eda6e
Armando <armandopaulinosilvaneto@gmail.com>
2021-06-26 17:25:07 -0300

    Criação de classe Service e reestruturação do código.

--- src/main/java/br/com/desafiodio/personapi/controller/PersonController.java
+++ src/main/java/br/com/desafiodio/personapi/controller/PersonController.java
@@ -2,24 +2,25 @@ package br.com.desafiodio.personapi.controller;
 
 import br.com.desafiodio.personapi.dto.response.MessageResponseDTO;
 import br.com.desafiodio.personapi.entity.Person;
-import br.com.desafiodio.personapi.repository.PersonRepository;
+import br.com.desafiodio.personapi.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/people")
 public class PersonController {
 
-    private PersonRepository personRepository;
+    private PersonService personService;
 
     @Autowired
-    public PersonController(PersonRepository personRepository){
-        this.personRepository = personRepository;
+    public PersonController(PersonService personService) {
+        this.personService = personService;
     }
 
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public MessageResponseDTO createPerson(@RequestBody Person person){
-        Person savePerson = personRepository.save(person);
-        return MessageResponseDTO.builder().message("Pessoa inserida com sucesso" + savePerson.getId()).build();
+        return personService.createPerson(person);
     }
 }
---
+++ src/main/java/br/com/desafiodio/personapi/service/PersonService.java
@@ -0,0 +1,25 @@
+package br.com.desafiodio.personapi.service;
+
+import br.com.desafiodio.personapi.dto.response.MessageResponseDTO;
+import br.com.desafiodio.personapi.entity.Person;
+import br.com.desafiodio.personapi.repository.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@Service
+public class PersonService {
+
+    private PersonRepository personRepository;
+
+    @Autowired
+    public PersonService(PersonRepository personRepository){
+        this.personRepository = personRepository;
+    }
+
+    public MessageResponseDTO createPerson(Person person){
+        Person savePerson = personRepository.save(person);
+        return MessageResponseDTO.builder().message("Pessoa inserida com sucesso " + savePerson.getId()).build();
+    }
+}
