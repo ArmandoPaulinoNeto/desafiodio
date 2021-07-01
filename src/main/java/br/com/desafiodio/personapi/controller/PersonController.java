3fcbbc09daca4719ff42ae8149969469cad1d88c
Armando <armandopaulinosilvaneto@gmail.com>
2021-06-30 21:08:15 -0300

    Criação da Classe PersonNotFoundException e dos métodos ListAll e findById.

--- src/main/java/br/com/desafiodio/personapi/controller/PersonController.java
+++ src/main/java/br/com/desafiodio/personapi/controller/PersonController.java
@@ -3,12 +3,14 @@ package br.com.desafiodio.personapi.controller;
 import br.com.desafiodio.personapi.dto.request.PersonDTO;
 import br.com.desafiodio.personapi.dto.response.MessageResponseDTO;
 import br.com.desafiodio.personapi.entity.Person;
+import br.com.desafiodio.personapi.exception.PersonNotFoundException;
 import br.com.desafiodio.personapi.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/people")
@@ -26,5 +28,14 @@ public class PersonController {
     @ResponseStatus(HttpStatus.CREATED)
     public MessageResponseDTO createPerson(@RequestBody @Valid PersonDTO personDTO){
         return personService.createPerson(personDTO);
+    }
+
+    @GetMapping("/{id}")
+    public PersonDTO findById(@PathVariable Long id) throws PersonNotFoundException {
+        return personService.findById(id);
+    }
+    @GetMapping
+    public List<PersonDTO> listAll(){
+        return personService.listAll();
     }
 }
--- src/main/java/br/com/desafiodio/personapi/service/PersonService.java
+++ src/main/java/br/com/desafiodio/personapi/service/PersonService.java
@@ -3,10 +3,16 @@ package br.com.desafiodio.personapi.service;
 import br.com.desafiodio.personapi.dto.request.PersonDTO;
 import br.com.desafiodio.personapi.dto.response.MessageResponseDTO;
 import br.com.desafiodio.personapi.entity.Person;
+import br.com.desafiodio.personapi.exception.PersonNotFoundException;
 import br.com.desafiodio.personapi.mapper.PersonMapper;
 import br.com.desafiodio.personapi.repository.PersonRepository;
+import net.bytebuddy.implementation.bytecode.Throw;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class PersonService {
@@ -20,9 +26,22 @@ public class PersonService {
         this.personRepository = personRepository;
     }
 
+    public List<PersonDTO> listAll() {
+        List<Person> allPeople = personRepository.findAll();
+        return allPeople.stream().map(personMapper::toDTO).collect(Collectors.toList());
+    }
+
     public MessageResponseDTO createPerson(PersonDTO personDTO){
         Person personToSave = personMapper.toModel(personDTO);
         Person savePerson = personRepository.save(personToSave);
         return MessageResponseDTO.builder().message("Pessoa inserida com sucesso " + savePerson.getId()).build();
+    }
+
+    public PersonDTO findById(Long id) throws PersonNotFoundException{
+        Optional<Person> opcionalPerson = personRepository.findById(id);
+        if(opcionalPerson.isEmpty()){
+            throw new PersonNotFoundException(id);
+        }
+        return personMapper.toDTO(opcionalPerson.get());
     }
 }
